15b9567c9b5871e6c2d2052f47dac22ed12187c5
Julien-bl <julien.bulin@epitech.digital>
2024-09-05 15:29:17 +0200

    Create ex_03.js

---
+++ ex_03.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+const adDescription = document.createElement("p")
+adDescription.innerHTML="Now featuring a headphone jack!";
+
+document.getElementById('description').appendChild(adDescription);
+});
